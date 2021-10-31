d55abf0f7135380d491ac6a8ee016344b888a005
NebelwerferT <NebelwerferT9@gmail.com>
2021-11-01 03:40:30 +0400

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const EOL = require("os").EOL;
+
+const execExit = () => {
+    console.log(`${EOL}До свидания!`);
+    process.exit(0);
+}
+
+const filePath = path.resolve(__dirname, 'text.txt');
+
+fs.open(filePath, 'w', (err) => {
+    if (err) throw err;
+    console.log('Введите текст: ');
+    rl.prompt();
+});
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: '> ',
+});
+
+rl.addListener('line', (data) => {
+    if (data.trim() === 'exit') execExit();
+    fs.appendFile(filePath, `${data}${EOL}`, (err) => {
+        if (err) throw err;
+    });
+    rl.prompt();
+});
+
+rl.addListener('SIGINT', () => {
+    console.log('Ctrl + C');
+    execExit();
+})
